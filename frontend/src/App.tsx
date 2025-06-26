c7498f2dfb2e3d7480eff2110a669b8b05021735
saiteja2873 <125501932+saiteja2873@users.noreply.github.com>
2025-06-26 15:40:24 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import SearchBar from './searchBar'
 import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
 import NavBar from './navBar'
