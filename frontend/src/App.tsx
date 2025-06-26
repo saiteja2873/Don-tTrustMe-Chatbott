7859dfd08ab43a0dd6648cfd5a0316a06af94d8d
saiteja2873 <125501932+saiteja2873@users.noreply.github.com>
2025-06-26 15:42:45 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import SearchBar from './searchBar'
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import {Routes, Route} from "react-router-dom"
 import NavBar from './navBar'
 
 function App() {
